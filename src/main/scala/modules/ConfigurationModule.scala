b877c6f36e1dffb34d6b48a398419faf8571975d
Ernesto Chero <ernesto@jemstep.com>
2019-11-18 17:14:53 -0500

    Added ConfigurationModule to volskaya kafka service

---
+++ src/main/scala/modules/ConfigurationModule.scala
@@ -0,0 +1,32 @@
+package modules
+import zio._
+import pureconfig._
+import pureconfig.generic.auto
+import ConfigurationModule._
+trait ConfigurationModule {
+  val configurationModule: Service[Any]
+}
+
+object ConfigurationModule {
+  case class ConfigurationError(message: String) extends RuntimeException(message)
+  case class Configuration(appName: String)
+  trait Service[R] {
+    def configuration: ZIO[R, Throwable, Configuration]
+  }
+
+  trait Live extends ConfigurationModule {
+    override val configurationModule: Service[Any] = new Service[Any] {
+      override def configuration: ZIO[Any, Throwable, Configuration] =
+        ZIO
+          .fromEither(
+            ConfigSource.default.load[Configuration]
+          )
+          .mapError(e => ConfigurationError(e.toList.mkString(", ")))
+    }
+  }
+
+  object factory extends Service[ConfigurationModule] {
+    override def configuration: ZIO[ConfigurationModule, Throwable, Configuration] =
+      ZIO.accessM[ConfigurationModule](_.configurationModule.configuration)
+  }
+}
